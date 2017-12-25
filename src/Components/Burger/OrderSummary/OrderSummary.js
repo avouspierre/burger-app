de41deec5f2fe88a97e17bed7e2d32282e52fc9a
avouspierre <pn.lagarde@gmail.com>
2017-12-25 18:06:19 +0100

    Performance improvement with less update

--- src/Components/Burger/OrderSummary/OrderSummary.js
+++ src/Components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingSummary = Object.keys(props.ingredients)
+
+class OrderSummary extends Component {
+    // This could be a functional component
+    // not a class required
+    componentWillUpdate() {
+            console.log('updated order summary');
+    }
+
+    render() {
+
+        const ingSummary = Object.keys(this.props.ingredients)
             .map(igKey => {
                 return (
                     <li key={igKey}>
-                        <span style={{textTransform:'capitalize'}}>{igKey} : </span> {props.ingredients[igKey]}
+                        <span style={{textTransform:'capitalize'}}>{igKey} : </span> {this.props.ingredients[igKey]}
             </li> ) 
             });
 
-    return (
-    <Aux>
+        return (
+            <Aux>
         <h3>Your order</h3>
         <p>A delicious hamburger with</p>
         <ul>
             {ingSummary}
         </ul>
-        <p>Total Price: <strong>{props.price.toFixed(2)}</strong></p>
+        <p>Total Price: <strong>{this.props.price.toFixed(2)}</strong></p>
         <p>Continue Checkout ?</p>
-        <Button clicked={props.cancelOrder} btnType='Danger'>CANCEL</Button>
-        <Button clicked={props.continueOrder} btnType='Success'>CONTINUE</Button>   
-    </Aux>);
+        <Button clicked={this.props.cancelOrder} btnType='Danger'>CANCEL</Button>
+        <Button clicked={this.props.continueOrder} btnType='Success'>CONTINUE</Button>   
+    </Aux>
+
+        )
+    }
+
 }
 
-export default orderSummary;
+export default OrderSummary;
--- src/Components/UI/Modal/Modal.js
+++ src/Components/UI/Modal/Modal.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.css';
 import BackDrop from '../BackDrop/BackDrop';
 import Aux from '../../../hoc/Aux';
 
-const modal = (props) => (
-    <Aux>
-        <BackDrop show={props.show} clicked={props.modalClosed}/>
-        <div className={classes.Modal}
-        style={{
-            transform: props.show ? 'translateY(0)':'translateY(-100vh)',
-            opacity: props.show ? '1':'0'
-        }}>
-            {props.children}
-        </div>
-    </Aux>
-)
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return  nextProps.show !== this.props.show;
+    }
 
-export default modal;
+    componentWillUpdate() {
+        console.log('Modal must updated');
+    }
+
+
+    render() {
+        return (
+            <Aux>
+                <BackDrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div className={classes.Modal}
+                style={{
+                    transform: this.props.show ? 'translateY(0)':'translateY(-100vh)',
+                    opacity: this.props.show ? '1':'0'
+                }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
+}
+
+export default Modal;
